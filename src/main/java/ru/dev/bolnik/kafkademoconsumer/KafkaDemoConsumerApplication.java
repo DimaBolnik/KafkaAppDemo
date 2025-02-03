183eb8ac9e43ef18e233ff6764330203beddd51f
User <dimabolnik@mail.ru>
2025-02-03 16:05:28 +0300

    KafkaAppDemo(0.1)

---
+++ src/main/java/ru/dev/bolnik/kafkademoconsumer/KafkaDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package ru.dev.bolnik.kafkademoconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemoConsumerApplication.class, args);
+    }
+
+}
